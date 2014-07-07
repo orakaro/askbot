c7d0f768952c2d6eb1987c5f2f7364a732cfbf3e
Vu Nhat Minh <vunhat_minh@dwango.co.jp>
2014-07-07 14:23:51 +0900

    stream

--- askbot/askbot.py
+++ askbot/askbot.py
@@ -1,5 +1,6 @@
 import os
 import os.path
+import argparse
 from .consumer import *
 
 from twitter.stream import *
@@ -34,3 +35,67 @@ def authen():
 def tweet():
     t = Twitter(auth=authen())
     t.statuses.update(status="Twitter said Py")
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description=__doc__ or "")
+    parser.add_argument(
+        '-to',
+        '--timeout',
+        help='Timeout for the stream (seconds).')
+    parser.add_argument(
+        '-ht',
+        '--heartbeat-timeout',
+        help='Set heartbeat timeout.',
+        default=90)
+    parser.add_argument(
+        '-nb',
+        '--no-block',
+        action='store_true',
+        help='Set stream to non-blocking.')
+    parser.add_argument(
+        '-tt',
+        '--track-keywords',
+        help='Search the stream for specific text.')
+    return parser.parse_args()
+
+def stream(args):
+
+    domain = 'userstream.twitter.com'
+    # These arguments are optional:
+    stream_args = dict(
+        timeout=args.timeout,
+        block=not args.no_block,
+        heartbeat_timeout=args.heartbeat_timeout)
+
+    # Track keyword
+    query_args = dict()
+    if args.track_keywords:
+        query_args['track'] = args.track_keywords
+
+    # Get stream
+    stream = TwitterStream(
+        auth=authen(),
+        domain=domain,
+        **stream_args)
+
+    tweet_iter = stream.user(**query_args)
+    for tweet in tweet_iter:
+        if tweet is None:
+            printNicely("-- None --")
+        elif tweet is Timeout:
+            printNicely("-- Timeout --")
+        elif tweet is HeartbeatTimeout:
+            printNicely("-- Heartbeat Timeout --")
+        elif tweet is Hangup:
+            printNicely("-- Hangup --")
+        elif tweet.get('text'):
+            printNicely(tweet.get('text'))
+
+def main():
+    args = parse_arguments()
+    stream = stream(args)
+
+
+if __name__ == 'main':
+    main()
