b24fb984d33f4c77160a86ceab277f20fb3a7f25
Vu Nhat Minh <vunhat_minh@dwango.co.jp>
2014-07-07 13:48:24 +0900

    init bot

---
+++ askbot.py
@@ -0,0 +1,34 @@
+from .consumer import *
+
+from twitter.stream import *
+from twitter.api import *
+from twitter.oauth import OAuth, read_token_file
+from twitter.oauth_dance import oauth_dance
+from twitter.util import printNicely
+
+
+def authen():
+    """
+    Authenticate with Twitter OAuth
+    """
+    # When using rainbow stream you must authorize.
+    twitter_credential = os.environ.get(
+        'HOME',
+        os.environ.get(
+            'USERPROFILE',
+            '')) + os.sep + '.askbot_oauth'
+    if not os.path.exists(twitter_credential):
+        oauth_dance("Dwango Ask",
+                    CONSUMER_KEY,
+                    CONSUMER_SECRET,
+                    twitter_credential)
+    oauth_token, oauth_token_secret = read_token_file(twitter_credential)
+    return OAuth(
+        oauth_token,
+        oauth_token_secret,
+        CONSUMER_KEY,
+        CONSUMER_SECRET)
+
+def tweet():
+    t = Twitter(auth=authen())
+    t.statuses.update(status="Twitter said Py")
